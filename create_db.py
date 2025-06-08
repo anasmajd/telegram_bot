1d0cdaf272134514a66a6d1a6397ebfac062bfc2
anasmajd <48318298+anasmajd@users.noreply.github.com>
2025-06-08 22:55:34 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-# فتح الاتصال بقاعدة البيانات
 conn = sqlite3.connect("referrals.db")
 c = conn.cursor()
 
-# إنشاء الجدول (لو لم يكن موجودًا)
 c.execute('''
     CREATE TABLE IF NOT EXISTS referrals (
         user_id INTEGER PRIMARY KEY,
